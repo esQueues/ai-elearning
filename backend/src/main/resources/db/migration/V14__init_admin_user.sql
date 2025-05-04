70a338feb1529c22ddbe38e98ce079785baf4673
Qusain Sayat <nomad1465QS@gmail.com>
2025-05-04 12:17:46 +0500

    fixed to 12 cost factor bcrypt

--- backend/src/main/resources/db/migration/V14__init_admin_user.sql
+++ backend/src/main/resources/db/migration/V14__init_admin_user.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (email, password, role, firstname, lastname, created_at)
 VALUES (
            'admin@gmail.com',
-           '$2a$12$WIcN3PGxuRLyyusRzn7UyeMHkkN60p9CD5o.WE8AFR4ml0Sk/80be',
+           '$2a$12$IiZS6y6bEPY96LYpM6XWqOpv.8NoQ.l33mBlYeWREFf8Q.lSWC/yC',
            'ADMIN',
            'Admin',
            'Adminov',
