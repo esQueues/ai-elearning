609cb9f7ac4a4381dce430146b15e81c3c015e87
Dilyara <dilyara2091@gmail.com>
2025-05-19 12:05:47 +0600

    Added spacer between navbar and content

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes';
 
-const App = () => (
-    <BrowserRouter>
-        <div className="app-container">
-            <div className="navbar-spacer" />
-            <AppRoutes />
-        </div>
-    </BrowserRouter>
-);
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div className="app-container">
+                {/* Spacer */}
+                <div style={{ height: "100px" }}></div>
+                
+                <AppRoutes />
+            </div>
+        </BrowserRouter>
+    );
+};
 
 export default App;
